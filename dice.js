cbb9bccc5a0c8b9f1deb7e6e47fc80dcc7569e41
Linh Le Kim <linhlekim@gmail.com>
2018-07-31 16:47:38 +0000

    finished work

--- dice.js
+++ dice.js
@@ -1,4 +1,16 @@
-var numbers = process.argv[2];
+//get user input
+var numbers = process.argv.slice(2);  //array
+var numberList = [];
 
-var randomNumber = Math.floor((Math.random() * 6) + 1);
-console.log(randomNumber);
+// number of times you have to roll
+for (var x = 0 ; x < numbers[0]; x++){
+      var randomNumber = Math.floor((Math.random() * 6) + 1);
+      // add random number to empty string
+      numberList.push(randomNumber);
+}
+
+console.log(numberList.join(", "));
+
+
+
+
